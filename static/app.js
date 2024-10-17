583a983923b887927fed6f17e23463360087b7d2
NewMayur <mayurgharat28@gmail.com>
2024-10-17 19:08:56 +0530

    Added firebase integration

--- static/app.js
+++ static/app.js
@@ -1,13 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyABYgFWBrewhLAJ3if2DkbQO2F1PvHowOw",
+    authDomain: "minimal-app-10a70.firebaseapp.com",
+    projectId: "minimal-app-10a70",
+    storageBucket: "minimal-app-10a70.appspot.com",
+    messagingSenderId: "880348373063",
+    appId: "1:880348373063:web:0c2495d1b206b223b71162"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 // Environment-specific API URL
 const apiUrls = {
-    development: 'https://minimal-app-dev-50348277979.us-central1.run.app/test',
-    staging: 'https://minimal-app-stag-50348277979.us-central1.run.app/test',
-    production: 'https://minimal-app-prod-50348277979.us-central1.run.app/test',
-    localhost: 'http://127.0.0.1:5000//test'
+    dev: 'https://minimal-app-dev-50348277979.us-central1.run.app/test',
+    stag: 'https://minimal-app-stag-50348277979.us-central1.run.app/test',
+    prod: 'https://minimal-app-prod-50348277979.us-central1.run.app/test'
 };
 
 // Determine the environment
-const environment = 'development'; // Change this to 'staging' or 'production' as needed
+const environment = 'dev'; // Change this to 'staging' or 'production' as needed
 
 // Fetch the message from the backend
 fetch(apiUrls[environment])
